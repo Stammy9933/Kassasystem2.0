e6e5f756d5de91d64f988703eb2c34781e72f7ef
Nicklas <itg.nicklas.johansson@gmail.com>
2021-10-19 15:52:52 +0200

    Finished tests for membership as well as added a value check and a test for it to Points

--- src/test/java/MembershipTest.java
+++ src/test/java/MembershipTest.java
@@ -7,9 +7,26 @@ class MembershipTest {
     @Test
     void discountSaved(){
         Membership m = new Membership(new Customer(new Money(1000)));
-        m.getPoints().addPoints(100);
+        m.getPoints().addPoints(100000);
         m.buyStaticDiscount();
-        //m.getPoints().addPoints(50);
-        assertEquals(new Discount())
+        assertEquals(new StaticDiscount(100, m), m.getDiscount());
+    }
+
+    @Test
+    void discountRemoved(){
+        Membership m = new Membership(new Customer(new Money(1000)));
+        m.getPoints().addPoints(100000);
+        m.buyStaticDiscount();
+        m.removeDiscount();
+        assertEquals(0, m.getDiscount().getDiscount());
+    }
+
+    @Test
+    void secondDiscountAdded(){
+        Membership m = new Membership(new Customer(new Money(1000)));
+        m.getPoints().addPoints(100000);
+        m.buyStaticDiscount();
+        m.getPoints().addPoints(200000);
+        assertThrows(IllegalStateException.class, m::buyStaticDiscount);
     }
 }
